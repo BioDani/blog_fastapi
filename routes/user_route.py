875435202a1a86ed993957104fa615ef3e726033
BioDani <dtejada@unal.edu.co>
2023-07-11 22:10:57 -0500

    connection to db in mongodb atlas
    Signed-off-by: BioDani <dtejada@unal.edu.co>

--- routes/user_route.py
+++ routes/user_route.py
@@ -1,21 +1,23 @@
 from fastapi import APIRouter
-
+from config.db import connection
+from schemas.user_schema import userEntity, usersEntity
+from models.user_model import User
 
 user = APIRouter()
 
 @user.get('/users' ,tags= ['Find all users'])
 def find_all_users():
-    return "Usuarios que esten en la app."
+    return usersEntity(connection.database.users.find())
 
 @user.get('/users/{id}' ,tags= ['Find user by id'])
 def find_user():
     return "Usuarios que esten en la app."
 
 @user.post('/users' ,tags= ['Create a new user'])
-def create_user():
-    return "Usuarios que esten en la app."
+def create_user( user: User):
+    new_user = dict(user)
 
-@user.put('/users/{id}',tags= ['Update a user'])
+@user.put('/users/{id}' ,tags= ['Update a user'])
 def update_user():
     return "Usuarios que esten en la app."
 
